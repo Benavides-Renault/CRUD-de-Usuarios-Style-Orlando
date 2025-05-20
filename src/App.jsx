2951b7d3b0f902b42e14a96b8decdedc9fd88d9d
Benavides-Renault <196871698+Benavides-Renault@users.noreply.github.com>
2025-05-19 22:37:00 -0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import Layout from './layout/Layout';
 
 function App() {
 	return (
